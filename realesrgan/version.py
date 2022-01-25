4757d0409d6c64609406604eb6d00458a3480105
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 23:10:24 +0300

    Fix

--- realesrgan/version.py
+++ realesrgan/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sat Jan  8 14:28:31 2022
+# TIME: Tue Jan 25 20:08:10 2022
 __version__ = '0.2.3.0'
 __gitsha__ = 'unknown'
 version_info = (0, 2, 3, 0)
--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import os
 
-os.system("chmod -R 0777 /model")
-
 with open("/model/run.sh", 'w') as f:
     f.write("cd model\n")
     f.write("python3 setup.py develop\n")
@@ -11,7 +9,6 @@ with open("/model/run.sh", 'w') as f:
         f.write(f'mkdir /results/{video}\n')
         f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --face_enhance --input /dataset/{video} --output /results/{video}\n')
 
-    f.write('chmod -R 0777 /results\n')
 
-os.system("chmod -R 0777 /model")
+os.system("chmod 0777 /model/run.sh")
 os.system("/model/run.sh")
