b20f7ac8b7e825ccc227582780057c956cf50e2a
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 23:17:36 +0300

    Fix SwinIr

--- realesrgan/version.py
+++ realesrgan/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Jan 25 20:08:10 2022
+# TIME: Tue Jan 25 20:16:20 2022
 __version__ = '0.2.3.0'
 __gitsha__ = 'unknown'
 version_info = (0, 2, 3, 0)
--- run_swinir.py
+++ run_swinir.py
@@ -1,7 +1,5 @@
 import os
 
-os.system("chmod -R 0777 /model")
-
 with open("/model/run_swinir.sh", 'w') as f:
     f.write("cd model\n")
     f.write("python3 setup.py develop\n")
@@ -11,7 +9,5 @@ with open("/model/run_swinir.sh", 'w') as f:
         f.write(f'mkdir /results/{video}\n')
         f.write(f'python3 /model/SwinIR/main_test_swinir.py --task real_sr --model_path /model/experiments/pretrained_models/003_realSR_BSRGAN_DFO_s64w8_SwinIR-M_x4_GAN.pth --folder_lq /dataset/{video} --scale 4 --save_dir /results/{video}\n')
 
-    f.write('chmod -R 0777 /results\n')
-
-os.system("chmod -R 0777 /model")
+os.system("chmod +x /model/run_swinir.sh")
 os.system("/model/run_swinir.sh")
