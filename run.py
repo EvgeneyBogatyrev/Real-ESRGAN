2ab91e2c46539e73c9a3ac71053e3d8a5f41d7cc
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-08-19 11:49:37 +0300

    Remove face enhance

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
 
 with open("/model/run.sh", 'w') as f:
-    f.write("cd model\n")
+    f.write("cd ../model\n")
     f.write("python3 setup.py develop\n")
 
     videos = os.listdir("/dataset")
     for video in videos:
         f.write(f'mkdir /results/{video}\n')
-        f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --face_enhance --input /dataset/{video} --output /results/{video}\n')
+        f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --input /dataset/{video} --output /results/{video}\n')
 
 
 os.system("chmod 0777 /model/run.sh")
