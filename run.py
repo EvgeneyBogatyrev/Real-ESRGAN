44d27605049ae6d3e3004a9a89c208830e0b6645
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-08-21 15:09:12 +0300

    Fix \n

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ with open("/model/run.sh", 'w') as f:
     for video in videos:
         f.write(f'mkdir /results/{video}\n')
         f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --input /dataset/{video} --tile 1024 --output /results/{video}\n')
-        f.write(f'ffmpeg -y -i /results/{video}/frame%04d.png -c:v libx264rgb -crf 0 /result/{video}.mp4')
-        f.write(f'rm -rf /result/{video}')
+        f.write(f'ffmpeg -y -i /results/{video}/frame%04d.png -c:v libx264rgb -crf 0 /result/{video}.mp4\n')
+        f.write(f'rm -rf /result/{video}\n')
 
 
 os.system("chmod 0777 /model/run.sh")
