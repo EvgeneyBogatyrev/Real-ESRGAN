6b8754388926f9ce0280cb0902fd3732640cea73
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-08-21 13:36:33 +0300

    COmpress

--- run.py
+++ run.py
@@ -7,7 +7,9 @@ with open("/model/run.sh", 'w') as f:
     videos = os.listdir("/dataset")
     for video in videos:
         f.write(f'mkdir /results/{video}\n')
-        f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --input /dataset/{video} --output /results/{video}\n')
+        f.write(f'python3 inference_realesrgan.py -n RealESRGAN_x4plus --input /dataset/{video} --tile 1024 --output /results/{video}\n')
+        f.write(f'ffmpeg -y -i /results/{video}/frame%04d.png -c:v libx264rgb -crf 0 /result/{video}.mp4')
+        f.write(f'rm -rf /result/{video}')
 
 
 os.system("chmod 0777 /model/run.sh")
